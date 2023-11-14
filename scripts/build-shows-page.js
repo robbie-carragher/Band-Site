9aae3141f3f9167279d15eb2529978db4ae163ce
robbie-carragher <orcafinweb@gmail.com>
2023-11-13 17:02:16 -0800

    Shows dynamic section styled and responsive design now complete.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -36,105 +36,86 @@ const showList = [
 
 const showListEl = document.querySelector(".shows__list");
 
-// const title = document.createElement('h2');
-// title.textContent = 'Shows';
-// showListEl.parentElement.insertBefore(title, showListEl);
-
-// // Create and append the Date, Venue, and Location titles
-// const dateTitle = document.createElement('p');
-// dateTitle.classList.add('show__title');
-// dateTitle.textContent = 'DATE';
-// dateTitle.style.textTransform = 'uppercase';
-// showListEl.appendChild(dateTitle);
-
-// const venueTitle = document.createElement('p');
-// venueTitle.classList.add('show__title');
-// venueTitle.textContent = 'VENUE';
-// venueTitle.style.textTransform = 'uppercase';
-// showListEl.appendChild(venueTitle);
-
-// const locationTitle = document.createElement('p');
-// locationTitle.classList.add('show__title');
-// locationTitle.textContent = 'LOCATION';
-// locationTitle.style.textTransform = 'uppercase';
-// showListEl.appendChild(locationTitle);
 
 // Create and insert the 'Shows' title
 const title = document.createElement('h2');
 title.textContent = 'Shows';
+title.classList.add('shows__main-title');
 showListEl.parentElement.insertBefore(title, showListEl);
 
-// // Create a container for the titles
-// const titlesContainer = document.createElement('div');
-// titlesContainer.classList.add('titles-container');
 
-// // Create and append the Date, Venue, and Location titles
-// const dateTitle = document.createElement('p');
-// dateTitle.classList.add('show__title');
-// dateTitle.textContent = 'DATE';
-// dateTitle.style.textTransform = 'uppercase';
-// titlesContainer.appendChild(dateTitle);
+// Create a div for the titles
+const titlesWrap = document.createElement('div');
+titlesWrap.classList.add('shows__title--wrap');
 
-// const venueTitle = document.createElement('p');
-// venueTitle.classList.add('show__title');
-// venueTitle.textContent = 'VENUE';
-// venueTitle.style.textTransform = 'uppercase';
-// titlesContainer.appendChild(venueTitle);
+// Create and append the Date, Venue, and Location titles
+const dateListTitle = document.createElement('p');
+dateListTitle.classList.add('shows__list-title');
+dateListTitle.textContent = 'DATE';
+dateListTitle.style.textTransform = 'uppercase';
+titlesWrap.appendChild(dateListTitle);
 
-// const locationTitle = document.createElement('p');
-// locationTitle.classList.add('show__title');
-// locationTitle.textContent = 'LOCATION';
-// locationTitle.style.textTransform = 'uppercase';
-// titlesContainer.appendChild(locationTitle);
+const venueListTitle = document.createElement('p');
+venueListTitle.classList.add('shows__list-title');
+venueListTitle.textContent = 'VENUE';
+venueListTitle.style.textTransform = 'uppercase';
+titlesWrap.appendChild(venueListTitle);
 
-// // Append the container to the parent of showListEl
-// showListEl.parentElement.insertBefore(titlesContainer, showListEl);
+const locationListTitle = document.createElement('p');
+locationListTitle.classList.add('shows__list-title');
+locationListTitle.textContent = 'LOCATION';
+locationListTitle.style.textTransform = 'uppercase';
+titlesWrap.appendChild(locationListTitle);
+
+// Insert the titles wrap under the main title
+showListEl.parentElement.insertBefore(titlesWrap, showListEl);
+
 
 for (let i = 0; i < showList.length; i++) {
   const showItem = document.createElement('li');
-  showItem.classList.add('show__item');
+  showItem.classList.add('shows__item');
 
   // Create and append the Date title
   const dateTitle = document.createElement('p');
-  dateTitle .classList.add('show__title');
+  dateTitle .classList.add('shows__title');
   dateTitle.textContent = 'DATE';
   dateTitle.style.textTransform = 'uppercase';
   showItem.appendChild(dateTitle);
 
   const showDate = document.createElement('p');
-  showDate.classList.add('show__details');
+  showDate.classList.add('shows__details');
   showDate.textContent = formatDate(showList[i].date);
   showItem.appendChild(showDate);
 
   // Create and append the Venue title
   const venueTitle = document.createElement('p');
-  venueTitle .classList.add('show__title');
+  venueTitle .classList.add('shows__title');
   venueTitle.textContent = 'VENUE';
   venueTitle.style.textTransform = 'uppercase';
   showItem.appendChild(venueTitle);
 
   const showVenue = document.createElement('p');
-  showVenue.classList.add('show__venue');
+  showVenue.classList.add('shows__venue');
   showVenue.textContent = showList[i].venue;
   showItem.appendChild(showVenue);
 
   // Create and append the Location title
   const locationTitle = document.createElement('p');
-  locationTitle.classList.add('show__title');
+  locationTitle.classList.add('shows__title');
   locationTitle.textContent = 'LOCATION';
   locationTitle.style.textTransform = 'uppercase';
   showItem.appendChild(locationTitle);
 
   const showLocation = document.createElement('p');
-  showLocation.classList.add('show__location');
+  showLocation.classList.add('shows__location');
   showLocation.textContent = showList[i].location;
   showItem.appendChild(showLocation);
 
   const colorButton = document.createElement('button');
   colorButton.textContent = 'BUY TICKETS';
-  colorButton.classList.add('color-change-btn');
+  colorButton.classList.add('shows__btn');
   colorButton.addEventListener('click', function(event) {
-    showItem.style.backgroundColor = 'lightblue'; 
+    showItem.style.backgroundColor = '#E1E1E1'; 
   });
   showItem.appendChild(colorButton);
 
